25be940c5fd1b30e9c37402ce58493847c09c525
GloRudd <fehmieren80@gmail.com>
2023-01-11 10:43:10 +0300

    commands added.

---
+++ Alper's Gambit/balance.py
@@ -0,0 +1,3 @@
+asd = {"username": "asd", "balance": 0}
+dsa = {"username": "dsa", "balance": 0}
+lkjsda = {"username": "lkjsda", "balance": 0}
--- Alper's Gambit/twelve.py
+++ Alper's Gambit/twelve.py
@@ -8,6 +8,7 @@ from random import randint as ri
 papers = [1,2,3,4,6,12]
 emre = []
 tan = []
+readyCheck = False
 
 def papersPlease(person=list, t=int):
     i = 2
@@ -21,48 +22,72 @@ def gambit(player,bot):
     deckB = bot[0]*bot[1]
     return deckP,deckB
 
+def getUser(username):
+    try:
+        with open("balance.py","r") as f:
+            pass
+    except:
+        with open("balance.py","a") as f:
+            f.write(str(user) + " = {'balance' : 100}")
+
+#            f.write(str(user) + " = {'balance' : " + str(money) + "}")
+
+user = input('Who dis?: ')
+
+help = f'[COMMANDS]\nq: Quit\nall-in: Bet all your money\nhelp: Shows commands\n/////////////////\n'
+
+print(help)
+
 while True:
     bal = open("balance.txt", "r+")
     money = int(bal.read())
-    bet = int(input(f'You have ${money} How much you want to bet?: '))
-    if bet>money:
-        print("You can't bet more money than you have.")
-        exit()
-    else:
-        pass
+    bet = input(f'You have ${money} How much you want to bet?: ')
+    try: bet = int(bet); readyCheck = True
+    except: 
+        if bet=='q': break
+        elif bet=='all-in': bet = money; readyCheck = True
+        elif bet=='help': print(help)
+        else: print('Please enter an amount.')
+    try: 
+        if bet>money:
+            print("You can't bet more money than you have.")
+        else:pass
+    except: pass
 
-    print('YOUR DECK')
-    print(papersPlease(emre,2))
-    papersPlease(tan,2)
-    check = input('Do you want to play or fold? (p/f): ')
-    if check == 'p':
-        play = gambit(emre,tan)
-        if play[0] > play[1]:
-            money += bet
-            print(f'You won ${bet}! New Balance: {money}')
-            print(f"Tan's Hand: {tan}")
-        if play[0] < play[1]:
-            money -= bet
-            print(f'You lost ${bet}! New Balance: {money}')
-            print(f"Tan's Hand: {tan}")
-        if play[0] == play[1]:
-            sq_sumE = int(emre[0])**2+int(emre[1])**2
-            sq_sumT = int(tan[0])**2+int(tan[1])**2
-            if sq_sumE > sq_sumT:
+    while readyCheck:
+        print('YOUR DECK')
+        print(papersPlease(emre,2))
+        papersPlease(tan,2)
+        check = input('Do you want to play or fold? (p/f): ')
+        if check == 'p':
+            play = gambit(emre,tan)
+            if play[0] > play[1]:
                 money += bet
                 print(f'You won ${bet}! New Balance: {money}')
                 print(f"Tan's Hand: {tan}")
-            if sq_sumE < sq_sumT:
+            if play[0] < play[1]:
                 money -= bet
                 print(f'You lost ${bet}! New Balance: {money}')
                 print(f"Tan's Hand: {tan}")
-    if check == 'f':
-        money -= int(bet/2)
-        print(f'Folded. You got your ${bet/2} back')
+            if play[0] == play[1]:
+                sq_sumE = int(emre[0])**2+int(emre[1])**2
+                sq_sumT = int(tan[0])**2+int(tan[1])**2
+                if sq_sumE > sq_sumT:
+                    money += bet
+                    print(f'You won ${bet}! New Balance: {money}')
+                    print(f"Tan's Hand: {tan}")
+                if sq_sumE < sq_sumT:
+                    money -= bet
+                    print(f'You lost ${bet}! New Balance: {money}')
+                    print(f"Tan's Hand: {tan}")
+        if check == 'f':
+            money -= int(bet/2)
+            print(f'Folded. You got your ${bet/2} back')
 
-    bal.truncate(0)
-    bal.seek(0)
-    bal.write(str(money))
+        bal.truncate(0)
+        bal.seek(0)
+        bal.write(str(money))
 
-    emre = []
-    tan = []
+        emre = []
+        tan = []
+        readyCheck = False
