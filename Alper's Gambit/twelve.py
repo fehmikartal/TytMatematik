de37d54edd5648a1562d64fb457856986161242a
GloRudd <fehmieren80@gmail.com>
2023-01-14 23:01:22 +0300

    accounts added.

--- Alper's Gambit/twelve.py
+++ Alper's Gambit/twelve.py
@@ -1,20 +1,19 @@
 # Alper 12 sayısının tüm doğal sayı bölenlerinin her birini ayrı ayrı kağıtlara yazarak bir torbaya atıyor. Emre ve Tan bu torbadan
 # ikişer tane kağıt çekiyor. Çektikleri kağıtların üzerinde yazan sayıların çarpımı büyük olan oyunu kazanıyor, eğer çarpımları eşit
-# ise kareleri toplamı büyük olan oyunu kazanıyor. Emre'nin torbadan çektiği kağıtların üzerinde yazan sayılar 3 ve 4 lduğuna göre
+# ise kareleri toplamı büyük olan oyunu kazanıyor. Emre'nin torbadan çektiği kağıtların üzerinde yazan sayılar 3 ve 4 olduğuna göre
 # oyunu Tan'ın kazanması için çekmesi gereken sayıların toplamı aşağıdakilerden hangisi olabilir?
 
 from random import randint as ri
+import json
 
-papers = [1,2,3,4,6,12]
-emre = []
-tan = []
-readyCheck = False
+papers = [1,2,3,4,6,12]; emre = []; tan = []; readyCheck = False
+
+# FUNCTIONS
 
 def papersPlease(person=list, t=int):
-    i = 2
-    while i>0:
+    while t>0:
         person.append(papers[ri(0,5)])
-        i -=1
+        t -=1
     return person
 
 def gambit(player,bot):
@@ -22,25 +21,34 @@ def gambit(player,bot):
     deckB = bot[0]*bot[1]
     return deckP,deckB
 
-def getUser(username):
-    try:
-        with open("balance.py","r") as f:
-            pass
-    except:
-        with open("balance.py","a") as f:
-            f.write(str(user) + " = {'balance' : 100}")
+# LOGIN
 
-#            f.write(str(user) + " = {'balance' : " + str(money) + "}")
+try:
+    with open('testList.txt','r') as f:
+        fileRead = f.read()
+        fileRead = fileRead.replace("\'", "\"")
+        user_list = json.loads(fileRead)
+except: 
+    user_list = {}
 
 user = input('Who dis?: ')
+
+# GET BALANCE OF USER / CREATE USER
+
+try:
+    print(f'Welcome {user}! You have ${user_list[user]}')
+except:
+    user_list[user] = 100
+    print(f'Welcome {user}! You have ${user_list[user]}')
 
 help = f'[COMMANDS]\nq: Quit\nall-in: Bet all your money\nhelp: Shows commands\n/////////////////\n'
 
 print(help)
 
+# MENU LOOP
+
 while True:
-    bal = open("balance.txt", "r+")
-    money = int(bal.read())
+    money = user_list[user]
     bet = input(f'You have ${money} How much you want to bet?: ')
     try: bet = int(bet); readyCheck = True
     except: 
@@ -53,6 +61,8 @@ while True:
             print("You can't bet more money than you have.")
         else:pass
     except: pass
+
+    # GAME
 
     while readyCheck:
         print('YOUR DECK')
@@ -84,9 +94,9 @@ while True:
             money -= int(bet/2)
             print(f'Folded. You got your ${bet/2} back')
 
-        bal.truncate(0)
-        bal.seek(0)
-        bal.write(str(money))
+        user_list[user] = money
+        with open('testList.txt','w') as f:
+            f.write(str(user_list))
 
         emre = []
         tan = []
