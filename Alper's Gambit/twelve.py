b564e064124d1ac3dae792ddea22b6c1eaa41628
GloRudd <fehmieren80@gmail.com>
2023-02-02 12:08:12 +0300

    v3

--- Alper's Gambit/twelve.py
+++ Alper's Gambit/twelve.py
@@ -21,6 +21,25 @@ def gambit(player,bot):
     deckB = bot[0]*bot[1]
     return deckP,deckB
 
+def setBalance():
+    with open('balList.txt','w') as f:
+            f.write(str(user_list)); f.close()
+
+def resultMsg(win):
+    if win == True:
+        print(f'You won ${bet}! New Balance: {money}')
+    elif win == False:
+        print(f'You lost ${bet}! New Balance: {money}')
+    print(f"Tan's Hand: {tan}")
+
+def giveMoney(donor, taker, donation):
+    if user_list[donor][0] >= donation:
+        user_list[donor][0] -= donation
+        user_list[taker][0] += donation
+        print('Money sent successfully!\n')
+    else: print("You can't donate more money than you have!\n")
+    setBalance()
+
 # LOGIN
 
 try:
@@ -31,30 +50,48 @@ try:
 except: 
     user_list = {}
 
+
 user = input('Who dis?: ')
+
 
 # GET BALANCE OF USER / CREATE USER
 
 try:
-    print(f'Welcome {user}! You have ${user_list[user]}')
+    print(f'Welcome {user}! You have ${user_list[user][0]}')
 except:
-    user_list[user] = 100
-    print(f'Welcome {user}! You have ${user_list[user]}')
+    user_list[user] = [500, 0, 0]
+    print(f'Welcome {user}! You have ${user_list[user][0]}')
 
-help = f'[COMMANDS]\nq: Quit\nall-in: Bet all your money\nhelp: Shows commands\n/////////////////\n'
+help = f"[COMMANDS]\nq: Quit\nall-in: Bet all your money\nwork: Work and get some money if you don't have any\nstats: View your statistics\ndonate: Donate your money to someone\nhelp: Shows commands\n/////////////////\n"
 
 print(help)
 
 # MENU LOOP
 
 while True:
-    money = user_list[user]
+    money = user_list[user][0]
+    total_won = user_list[user][1]
+    total_lost = user_list[user][2]
     bet = input(f'You have ${money} How much you want to bet?: ')
     try: bet = int(bet); readyCheck = True
     except: 
         if bet=='q': break
         elif bet=='all-in': bet = money; readyCheck = True
         elif bet=='help': print(help)
+        elif bet=='work':
+            if money==0:
+                user_list[user][0] += ri(0,300)
+                setBalance()
+            else: print('You still have money. To be able to work, you need to have $0.')
+        elif bet=='stats':
+            print(f'Current Balance: {money}\nTotal Moneys Won: {total_won}\nTotal Moneys Lost: {total_lost}')
+        elif bet=='donate':
+            donateTaker = input('\nWrite the name of the person you want to give money to: ')
+            donateAmount = int(input('How much money do you want to send?: '))
+            try:
+                giveMoney(user, donateTaker, donateAmount)
+            except:
+                print(f"Couldn't found someone named {donateTaker}\n")
         else: print('Please enter an amount.')
     try: 
         if bet>money:
@@ -73,31 +110,31 @@ while True:
             play = gambit(emre,tan)
             if play[0] > play[1]:
                 money += bet
-                print(f'You won ${bet}! New Balance: {money}')
-                print(f"Tan's Hand: {tan}")
+                total_won += bet
+                resultMsg(True)
             if play[0] < play[1]:
                 money -= bet
-                print(f'You lost ${bet}! New Balance: {money}')
-                print(f"Tan's Hand: {tan}")
+                total_lost += bet
+                resultMsg(False)
             if play[0] == play[1]:
                 sq_sumE = int(emre[0])**2+int(emre[1])**2
                 sq_sumT = int(tan[0])**2+int(tan[1])**2
                 if sq_sumE > sq_sumT:
                     money += bet
-                    print(f'You won ${bet}! New Balance: {money}')
-                    print(f"Tan's Hand: {tan}")
-                if sq_sumE < sq_sumT:
+                    total_won += bet
+                    resultMsg(True)
+                elif sq_sumE < sq_sumT:
                     money -= bet
-                    print(f'You lost ${bet}! New Balance: {money}')
-                    print(f"Tan's Hand: {tan}")
+                    total_lost += bet
+                    resultMsg(False)
+                else:
+                    print('DRAW')
         if check == 'f':
             money -= int(bet/2)
+            total_lost += int(bet/2)
             print(f'Folded. You got your ${bet/2} back')
 
-        user_list[user] = money
-        with open('balList.txt','w') as f:
-            f.write(str(user_list))
+        user_list[user][0] = money ; user_list[user][1] = total_won ; user_list[user][2] = total_lost
+        setBalance()
 
-        emre = []
-        tan = []
-        readyCheck = False
+        emre = []; tan = []; readyCheck = False
